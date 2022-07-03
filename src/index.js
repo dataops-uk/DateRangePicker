cb4e36f64b3033507d6de74797ca2471fc734d24
Orlando <orlando@dataops.co.uk>
2022-07-03 17:25:47 +0100

    Modified to run in a popover

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-
-
 window.setupPicker = (json) => {
     const obj = JSON.parse(json);
     const {startDate, endDate, singlePicker, showCalendar, timePicker} = obj;
     const pickerOptions = {
-        startDate: startDate,
+                startDate: startDate,
         endDate: endDate,
         minYear: 2010,
         maxYear: parseInt(moment().format('YYYY'),10),
         showWeekNumbers: true,
         showDropdowns: false,
         singleDatePicker: singlePicker,
-        timePicker: timePicker,
-        timePicker24Hour: true,
+        // timePicker: timePicker,
+        // timePicker24Hour: true,
         alwaysShowCalendars: showCalendar,
         ranges: {
             'Today': [moment(), moment()],
@@ -23,10 +21,15 @@ window.setupPicker = (json) => {
             'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         },
         locale: {
-            format: 'DD/MM/YYYY' // hh:mm a'
-        }
+            applyLabel: 'Submit', // Change the button label from 'Apply'
+            cancelLabel: 'Clear', // Change the button label from 'Cancel'
+            format: 'DD/MM/YYYY'
+        }  
+
     };
 
+
+// Capture the response after date(s) picked and applied
 $("#datepicker").daterangepicker(
     pickerOptions ,
     function(start, end, label) {
@@ -36,17 +39,20 @@ $("#datepicker").daterangepicker(
 );
 
 
+// Run a script when the Cancel button is pressed
+$('#datepicker').on('cancel.daterangepicker', function(ev, picker) {
+    FileMaker.PerformScript("Cancel", JSON.stringify(obj) )
+});
+
+
+// Run a script when the picker is closed ousside of the Cancel or Apply buttons, background click
+$('#datepicker').on('hide.daterangepicker', function(ev, picker) {
+    FileMaker.PerformScript("Close", JSON.stringify(obj) )
+});
+
+
+// added this line to pop open the calendar as soon as the function is run
 document.getElementById('datepicker').click();
 
 
 };
-
-
-window.setDateRange = (json) => {
-    const obj = JSON.parse(json);
-    const {startDate, endDate} = obj;
-    console.log (startDate);
-    $('#daterange').data('daterangepicker').setStartDate(startDate);
-
-};
-
